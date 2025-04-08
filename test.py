6c5c34e26ae5b764ea4bd5880e81dd2989d9c642
FrankAffatigato <ifrankdavida@gmail.com>
2025-04-08 10:39:27 -0500

    Finalized version 1 of todo web app and created requirements, so it is ready to publish

--- test.py
+++
@@ -1,6 +0,0 @@
-import functions as f
-
-todos = f.get_todos()
-
-for todo in todos:
-    st.checkbox(todo)
--- web.py
+++ web.py
@@ -15,10 +15,17 @@ st.subheader("This is my todo app.")
 st.write("This app is built to increase your productivity")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        f.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="Enter a todo:", placeholder="Schedule appointment",
               on_change=add_todo, key='new_todo')
 
-print("hello")
+# print("hello")
+# st.session_state
